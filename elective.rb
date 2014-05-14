a5608f3761a967deaed84f6ec5c0c694c565dcce
vicfriedman <victoria.m.friedman@gmail.com>
2014-05-14 10:47:03 -0400

    merge conflict

--- elective.rb
+++ elective.rb
@@ -1,1 +1,5 @@
 #code your solutions here
+
+def elective_array
+  electives = ["photography", "ceramics", "music", "robotics", "web development", "independant studies"]
+end
--- spec/elective_spec.rb
+++ spec/elective_spec.rb
@@ -1,1 +1,9 @@
 require 'spec_helper'
+
+describe 'Elective' do 
+
+  describe '#elective_array' do
+    expect(elective_array[0]).to eq "photography"
+  end
+  
+end

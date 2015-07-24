2fc05afda9acc4bf44fa5ad98bb86f1a78c9d977
Dammy Akinfenwa <dammy.akinfenwa@gmail.com>
2015-07-24 00:27:47 +0000

    adds add'l array and associated tests. prevents hardcoding of answers.

--- spec/elective_spec.rb
+++ spec/elective_spec.rb
@@ -4,49 +4,61 @@ describe 'STRING ARRAY METHODS' do
 
   before(:each) do
     @electives = ["photography", "ceramics", "music", "robotics", "web development", "independent study"]
+		@electives_v2 = ["drama","art history","spanish","yearbook"]
   end
 
   describe '#count_items' do
     it 'returns the count of the items in an array' do
       expect(count_items(@electives)).to eq(6)
+			expect(count_items(@electives_v2)).to eq(4)
     end
   end  
 
   describe '#first_item' do
     it 'returns the first item in a given array' do
       expect(first_item(@electives)).to eq("photography")
+			expect(first_item(@electives_v2)).to eq("drama")
     end
   end 
 
   describe '#last_item' do
     it 'returns the last item in a given array' do
       expect(last_item(@electives)).to eq("independent study")
+			expect(last_item(@electives_v2)).to eq("yearbook")
     end
   end 
 
   describe '#alphabetical_first' do
     it 'returns the first item when an array is alphabetized' do
       expect(alphabetical_first(@electives)).to eq("ceramics")
+			expect(alphabetical_first(@electives_v2)).to eq("art history")
     end
   end 
 
   describe '#random_item' do
     it 'returns a random item a given array' do
       expect(["photography", "ceramics", "music", "robotics", "web development", "independent study"]).to include(random_item(@electives))
+			expect(["drama","art history","spanish","yearbook"]).to include(random_item(@electives_v2))
     end
   end 
 
   describe '#remove_item' do
-    it 'pops off independent study and prints the array' do
+		it 'pops off the last item in the array and prints the array' do
       expect($stdout).to receive(:puts).with(["photography", "ceramics", "music", "robotics", "web development"])
       remove_item(@electives)
+			
+			expect($stdout).to receive(:puts).with(["drama","art history","spanish"])
+			remove_item(@electives_v2)
     end
   end
 
   describe '#add_item' do
-    it 'pushes debate club onto the array then prints the array' do
+		it 'pushes debate club onto the array then prints the array' do
       expect($stdout).to receive(:puts).with(["photography", "ceramics", "music", "robotics", "web development", "independent study", "debate club"])
       add_item(@electives, "debate club")
+
+			expect($stdout).to receive(:puts).with(["drama","art history","spanish","yearbook","debate club"])
+			add_item(@electives_v2,"debate club")
     end
   end
 
@@ -58,14 +70,20 @@ describe 'STRING ARRAY METHODS' do
       expect($stdout).to receive(:puts).with("4. robotics")
       expect($stdout).to receive(:puts).with("5. web development")
       expect($stdout).to receive(:puts).with("6. independent study")
-
       print_items(@electives)
+
+			expect($stdout).to receive(:puts).with("1. drama")
+      expect($stdout).to receive(:puts).with("2. art history")
+      expect($stdout).to receive(:puts).with("3. spanish")
+			expect($stdout).to receive(:puts).with("4. yearbook")
+			print_items(@electives_v2)
     end
   end
 
   describe '#reverse_each' do
     it 'returns an array where each item has been reversed' do
       expect(reverse_each(@electives)).to eq(["yhpargotohp", "scimarec", "cisum", "scitobor", "tnempoleved bew", "yduts tnednepedni"]) 
+			expect(reverse_each(@electives_v2)).to eq(["amard","yrotsih tra","hsinaps","koobraey"])
     end
   end
 
